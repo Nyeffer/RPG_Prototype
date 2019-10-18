a9336b7c1000661e9f14781155d468f191b38427
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-10-17 20:25:11 -0400

    Player
    Setting up the Stats of Player.
    
    - Hp, will be constantly Update every Important step like Damage and Return to Map.
    
    - Includes Current Health and Maximum Health.

---
+++ RPG_Prototype/Assets/Scripts/BattleSystem/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Private Variables
+    private int maxHp = 0; // Maximum Health, required for healing
+    private int curHp; // Health that interacts with Damage and Heal
+
+    // Public Variables
+    public int hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Setters
+    public void SetHp(int health) {
+        hp = health;
+    }
+
+    // Getters
+    public int GetCurHp() {
+        return curHp;
+    }
+
+    public int GetMaxHp() {
+        return maxHp;
+    }
+}
