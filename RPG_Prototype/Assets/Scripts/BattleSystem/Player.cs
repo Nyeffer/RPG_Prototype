21433a90fa9e133d8fc702ade26bcf086dc7213e
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-10-18 14:51:31 -0400

    Added more Stats
    Added:
    - Atk value
       - Value that determine Damage to Enemies
     - Get Function
    - Def Value
      - Value that decrease Damage Taken
     - Get Function

--- RPG_Prototype/Assets/Scripts/BattleSystem/Player.cs
+++ RPG_Prototype/Assets/Scripts/BattleSystem/Player.cs
@@ -6,9 +6,14 @@ public class Player : MonoBehaviour
 {
     // Public Variables
     public int maxHp; // Maximum Health, required for healing
+    public float atkGrowth; // decimal number that multiplies the Atk stat per Level count
+    public float defGrowth; // decimal number that multiplies the Def Stat per Level count
 
     // Private Variables
     private int curHp; // Health that interacts with Damage and Heal
+    private int Level; // the integer that Stats are multiplied from
+    private int atkVal; // the integer that's actively use in Damage counting
+    private int defVal; // The integer that's actively use to decrease Damage taken 
 
     
     // Start is called before the first frame update
@@ -22,11 +27,24 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    // Level up
+    public void LevelUp() {
+
+    }
    
 
     // Getters
     public int GetHp() { // Function to GET assess to the CurrentHp integer
         return curHp;
+    }
+
+    public int GetAtk() { // Function to GET assess to the AtkVal integer for Damage Counting
+        return atkVal;
+    }
+
+    public int GetDef() { // Function to GET assess to the DefVal integer to decrease Damage taken
+        return defVal;
     }
     
 
